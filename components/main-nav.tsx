b40bbd3e65ac71dc4ac66321832b0af6dabcdc17
Eric Nichols <ebn646@hotmail.com>
2024-07-22 22:47:19 -0400

    adjusted main nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -19,93 +19,30 @@ import {
 import {components} from "@/config/constants"
 export const MainNav = () => {
   return (
-    <NavigationMenu className="hidden lg:flex">
-      <NavigationMenuList>
-      <NavigationMenuItem>
-          <Link href="/" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Home
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="hidden lg:grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <Icons.logo className="h-6 w-6" />
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components that you can copy and
-                      paste into your apps. Accessible. Customizable. Open
-                      Source.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-   
-      </NavigationMenuList>
-    </NavigationMenu>
-    // <div className="hidden lg:flex w-full justify-between">
-    //   <div className="flex gap-2">
-    //     <Link
-    //       href="/"
-    //       className="group flex w-full items-center gap-x-2.5"
-    //     >
-    //       <div className="size-9">
-    //        <Logo />
-    //       </div>
-    //       <h5 className="font-semibold">Enterprise</h5>
-    //     </Link>
-    //     <ComponentsDropdown />
-    //     <LayoutsDropdown />
-    //   </div>
-    //   <div className="flex gap-2 items-center">
-    //     <User />
-    //     <ThemeToggle />
-    //     <div>
-    //       <SearchIcon />
-    //     </div>
-    //     <div>
-    //       <GithubIcon />
-    //     </div>
-    //   </div>
-    // </div>
+    <div className="hidden lg:flex w-full justify-between">
+      <div className="flex gap-2">
+        <Link
+          href="/"
+          className="group flex w-full items-center gap-x-2.5"
+        >
+          <div className="size-9">
+           <Logo />
+          </div>
+          <h5 className="font-semibold">Enterprise</h5>
+        </Link>
+        <Link href="/components/buttons">Components</Link>
+      </div>
+      <div className="flex gap-2 items-center">
+        <User />
+        <ThemeToggle />
+        <div>
+          <SearchIcon />
+        </div>
+        <div>
+          <GithubIcon />
+        </div>
+      </div>
+    </div>
   );
 };
 
