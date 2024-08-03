93623b30a8d24d61081050f08bfecb7a41b659fc
Eric Nichols <ebn646@hotmail.com>
2024-08-03 17:23:44 -0400

    build error fixes

--- components/chat/chat.tsx
+++ components/chat/chat.tsx
@@ -1,10 +1,6 @@
 "use client";
 import {Message, UserData} from "@/data";
-import React, { use, useRef } from "react";
-import { Textarea } from "../ui/textarea";
-import { SendHorizontal } from "lucide-react";
-import { Button } from "../ui/button";
-import { AnimatePresence, motion } from "framer-motion";
+import React, { useRef } from "react";
 import { ChatList } from "./chat-list";
 
 type ChatProps = {
@@ -24,45 +20,6 @@ const Chat = ({messages, selectedUser, isMobile}: ChatProps) => {
     setMessages([...userMessages, newMessage]);
   };
 
-  // React.useEffect(() => {
-  //   const messages = Array.from({ length: 10 }, (_, i) => `Chat Message ${i}`);
-  //   setMessages(messages);
-  //   console.log("messages", messages);
-  // }, []);
-  // // set the container ref to scroll to the bottom when a new message is added
-  // React.useEffect(() => {
-  //   if (containerRef.current) {
-  //     containerRef.current.scrollTop = containerRef.current.scrollHeight;
-  //   }
-  // }, [messages]);
-
-  // create a submit function to add a new message
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    const formData = new FormData(form);
-    const message = formData.get("message") as string;
-    //setMessages([...messages, message]);
-
-    // clear the form
-    form.reset();
-  };
-
-  const handleSend = () => {
-    if (inputRef.current) {
-      const message = inputRef.current.value;
-      //setMessages([...messages, message]);
-      inputRef.current.focus();
-    }
-    formRef.current?.reset();
-  };
-
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (event.key === "Enter" && !event.shiftKey) {
-      event.preventDefault();
-      handleSend();
-    }
-  };
 
   return (
     <div className="flex flex-col justify-between w-full h-full">      
--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,42 +1,12 @@
-import { rest } from 'msw'
-import { apiURL } from '@/config'
-
+import { http } from 'msw'
+const apiURL = ''
 export const handlers = [
-  rest.get(`${apiURL}/scoops`, (req, res, ctx) => {
-    return res(
-      ctx.json([
-        { name: 'Chocolate', imagePath: '/images/chocolate.png' },
-        { name: 'Vanilla', imagePath: '/images/vanilla.png' },
-      ])
-    )
+  http.get('/resource', ({ cookies }) => {
+    const { token } = cookies
   }),
 
-  rest.get(`${apiURL}/toppings`, (req, res, ctx) => {
-    return res(
-      ctx.json([
-        {
-          name: 'Cherries',
-          imagePath: '/images/cherries.png',
-        },
-        {
-          name: 'M&Ms',
-          imagePath: '/images/m-and-ms.png',
-        },
-        {
-          name: 'Hot fudge',
-          imagePath: '/images/hot-fudge.png',
-        },
-      ])
-    )
-  }),
-
-  rest.post(`${apiURL}/order`, (req, res, ctx) => {
-    const orderNumber = 1234567890
-
-    return res(
-      ctx.json({
-        orderNumber,
-      })
-    )
-  }),
+  http.post('/user', async ({ request }) => {
+    // Read the request body as JSON.
+    const user = await request.json()
+  })
 ]
